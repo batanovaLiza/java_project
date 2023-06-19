32596665bca9f08cf902bacf4f55c74bd375ef49
Elizaveta Savkova <34881151+batanovaLiza@users.noreply.github.com>
2023-06-19 23:26:20 +0300

    Point2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -7,7 +7,14 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-
+    public double distance(){
+        Point p1 = new Point(2,0);
+        Point p2 = new Point(5,4);
+        double x0 = Math.pow((p1.x-p2.x), 2);
+        double y0 = Math.pow((p1.y-p2.y), 2);
+        double rasst = Math.sqrt(x0 + y0);
+        return (rasst);
+    }
 }
 
 
