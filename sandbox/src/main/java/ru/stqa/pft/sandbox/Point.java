94e9a4907c21809f0373693cd54125e5cb0524e4
Elizaveta Savkova <34881151+batanovaLiza@users.noreply.github.com>
2023-06-20 11:58:43 +0300

    Point tests

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -7,9 +7,7 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-    public double distance(){
-        Point p1 = new Point(2,0);
-        Point p2 = new Point(5,4);
+    public double distance(Point p1,Point p2){
         double x0 = Math.pow((p1.x-p2.x), 2);
         double y0 = Math.pow((p1.y-p2.y), 2);
         double rasst = Math.sqrt(x0 + y0);
--- sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/myFirstProgram.java
@@ -2,7 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class myFirstProgram {
     public static void main(String[] args) {
-        Point p1=new Point(3,4);
-        System.out.println("Расстояние между точками равно "+p1.distance());
+        Point p1 = new Point(2,0);
+        Point p2 = new Point(5,4);
+        System.out.println("Расстояние между точками равно "+p1.distance(p1,p2));
     }
 }
