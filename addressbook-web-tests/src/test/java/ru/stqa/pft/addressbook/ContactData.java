0b4bc70d0b172d0b0514c958ee09407f76908dee
Elizaveta Savkova <34881151+batanovaLiza@users.noreply.github.com>
2023-06-30 13:09:16 +0300

    Задание 4 NewContactCreation

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String lastName;
+    private final String company;
+    private final String address;
+    private final String homePhoneNumb;
+    private final String email;
+
+    public ContactData(String firstName, String lastName, String company, String address, String homePhoneNumb, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.company = company;
+        this.address = address;
+        this.homePhoneNumb = homePhoneNumb;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHomePhoneNumb() {
+        return homePhoneNumb;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
